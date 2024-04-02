e5ed4bd1d2705a87a0f7a9f64b42a72797a100c8
sunpeize666 <144121626+sunpeize666@users.noreply.github.com>
2024-04-02 13:46:21 +0900

    Add files via upload

---
+++ 04.02_02_손패택.py
@@ -0,0 +1,18 @@
+'''
+202195057 손패택
+2024년04월02일
+'''
+month = int(input("월을 입력하시오 : "))
+if month== 3 or month == 4 or month == 5 :
+    print(month, "는 봉닙니다")
+elif month== 6 or month == 7 or month == 8:
+    print(month, "는 여름닙니다")
+elif month== 9 or month == 10 or month == 11 :
+    print(month, "는 가을니다")
+elif month== 1 or month == 12 or month == 2 :
+    print(month, "는 겨울닙니다")
+else :
+    print("해당 월은 없습니다.")
+
+
+
